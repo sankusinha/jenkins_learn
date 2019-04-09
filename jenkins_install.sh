08ed0507b051380ccea855c8819edc2758cb3cb8
sankusinha <sanku.devops@gmail.com>
2019-04-09 10:16:51 +0000

    jenkins installation script

---
+++ jenkins_install.sh
@@ -0,0 +1,9 @@
+sudo yum update –y
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-x64.rpm
+sudo yum install -y jdk-8u201-linux-x64.rpm
+rm jdk-8u201-linux-x64.rpm
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum install jenkins -y
+sudo service jenkins start
+            
